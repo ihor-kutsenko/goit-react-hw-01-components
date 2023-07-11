9d2b6e3c75a7c85939e3d3eda0b7981eb67f61fd
Rammbest <igor-kutsenko@ukr.net>
2023-07-12 00:01:17 +0300

    add FriendList and TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,10 @@ import user from './Profile/user.json';
 import Profile  from './Profile/Profile';
 import data from './Statistics/data.json';
 import Statistics  from './Statistics/Statistics';
+import friends from './FriendList/friends.json';
+import FriendList from './FriendList/FriendList';
+import transactions from './TransactionHistory/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
  const App = () => {
   return (
@@ -15,6 +19,8 @@ import Statistics  from './Statistics/Statistics';
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
